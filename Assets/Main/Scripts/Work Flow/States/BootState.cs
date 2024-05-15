40e6f37cf4a97c3c9b462f8348a427390b790964
Mitch Pen <gig405@mail.ru>
2024-05-15 03:19:22 +0300

    update: update screen setup in BootState

--- Assets/Main/Scripts/Work Flow/States/BootState.cs
+++ Assets/Main/Scripts/Work Flow/States/BootState.cs
@@ -4,16 +4,25 @@ namespace Main.Scripts.Work_Flow.States
 {
     public class BootState : State
     {
-        private readonly (int width, int heigh) _resolution = new (600,800) ;
+        private readonly (int width, int height) _screenResolution = new (600,800) ;
         public BootState(StateData data) : base(data) { }
 
         public override void Entire()
         {
             base.Entire();
+            SetupScreen();
+        }
+
+        private void SetupScreen()
+        {
             var screenMode = FullScreenMode.Windowed;
+            var windowPosition = Vector2Int.zero;
+            windowPosition.x = (int)(Screen.currentResolution.width / 2 - _screenResolution.width / 2);
+            windowPosition.y = (int)(Screen.currentResolution.height / 2 - _screenResolution.height / 2);
             Screen.fullScreenMode = screenMode;
-            Screen.SetResolution(_resolution.width, _resolution.heigh, screenMode, 
+            Screen.SetResolution(_screenResolution.width, _screenResolution.height, screenMode, 
                 Screen.currentResolution.refreshRateRatio);
+            Screen.MoveMainWindowTo(Screen.mainWindowDisplayInfo,windowPosition);
             QualitySettings.vSyncCount = 1;
         }
 
